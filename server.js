4063b4fb0367148ab1a37a8759769164f82273a1
Georgios Mpovolis <gam154@scarletmail.rutgers.edu>
2020-09-02 13:16:34 -0400

    updated readme and updated main.js logic

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const passport = require("./config/passport");
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
-require('dotenv').config()
+require('dotenv').config();
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
